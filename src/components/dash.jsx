90ba354c37e57ed2f6f95ad6391f3fc27a9db246
Alyssa Koh <a9koh@stanford.edu>
2024-03-13 02:23:54 -0700

    added more styling to the dasdhboard

--- src/components/dash.jsx
+++ src/components/dash.jsx
@@ -1,19 +1,47 @@
 import './dash.css';
 
+function Post() {
+  return (
+    <div className='post'>
+      i am a post
+    </div>
+  );
+}
+
+function Dashfeed() {
+  return (
+    <div className='dash-feed'>
+      i am the feed
+      <Post />
+      <Post />
+      <Post />
+    </div>
+  )
+}
+
+function Dashsidebar() {
+  return (
+    <div className='dash-sidebar'>
+      i am the sidebar
+    </div>
+  );
+}
+
+function Dashbar() {
+  return (
+    <div className='dash-bar'>
+      Alyssa's super duper cool blogging platform
+    </div>
+  )
+}
+
 export default function Dash() {
   return (
     <div className='dashboard'>
-      <div className='dash-bar'>
-        i am the dashbar
-      </div>
+      <Dashbar />
       <div className='dash-body'>
-        i am the dashbody
-        <div className='dash-feed'>
-          i am the feed
-        </div>
-        <div className='dash-sidebar'>
-          i am the sidebar
-        </div>
+        <Dashfeed />
+        <Dashsidebar />
       </div>
     </div>
   )
